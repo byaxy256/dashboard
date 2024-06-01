6cbb364a10938dc2052b6990ec6c4faf3fcdfc1a
Mwesigwa_Moses256 <byaxydraxler256@gmail.com>
2024-06-01 14:58:18 +0300

    On branch main

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+// import video from '../../Assets'
+const home = () => {
+  return (
+    <section className='home'>
+      <div className='overlay'></div>
+      {/* <video src={video} type='video/mp4'></video> */}
+    </section>
+  )
+}
+
+export default home
